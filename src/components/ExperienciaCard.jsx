76ad9e8c45528416208616d016ca60275c6992e5
CarlosDaniel-090304 <carlosdanielchilcon19@gmail.com>
2026-01-08 09:37:29 -0500

    Creando componente ExperienciaCard

---
+++ src/components/ExperienciaCard.jsx
@@ -0,0 +1,11 @@
+function ExperienciaCard({ name, period, image }){
+    return(
+        <div className='flex flex-col items-center'>
+            <img src={image} alt="Soluciones Integrales JB" className='rounded-full w-36 h-36 border-2 border-gray-200 mb-2'/>
+            <h1 className='w-full text-center mb-1'>{name}</h1>
+            <span className='text-[12px] text-zinc-400'>{period}</span>
+        </div>
+    )
+}
+
+export default ExperienciaCard
--- src/components/Inicio/Experiencia.jsx
+++ src/components/Inicio/Experiencia.jsx
@@ -3,41 +3,31 @@ import React from 'react';
 import BeniElectricSac from '../../assets/BeniElectricSac.png';
 import SolucionesIntegralesJB from '../../assets/SolucionesIntegralesJB.jpeg';
 
+// Componentes
+import ExperienciaCard from '../ExperienciaCard';
+
 function Experiencia(){
     return(
         <div className='flex flex-col gap-5'>
             {/* Titulo */}
-            <div className='text-lg text-gray-200 pb-2 mb-2 border-b border-zinc-700'>Experiencia</div>
+            <div className='text-lg text-gray-200 pb-2 mb-2 border-b border-zinc-700'>
+                Experiencia
+            </div>
 
             {/* Experiencia */}
             <div className='flex justify-around text-gray-200 gap-3'>
-                <div className='flex flex-col items-center'>
-                    <img src={BeniElectricSac} alt="Beni Electric SAC" className='rounded-full w-36 h-36 border-2 border-gray-200 mb-2'/>
-                    <h1>Corporación Beni Electric SAC</h1>
-                    <span className='text-sm'>1 sept 2025 - 16 dic 2025</span>
-                </div>
+                <ExperienciaCard 
+                    name={"Corporación Beni Electric SAC"}
+                    period={"1 sept 2025 - 16 dic 2025"}
+                    image={BeniElectricSac}
+                />
 
-                <div className='flex flex-col items-center'>
-                    <img src={SolucionesIntegralesJB} alt="Soluciones Integrales JB" className='rounded-full w-36 h-36 border-2 border-gray-200 mb-2'/>
-                    <h1>Soluciones Integrales JB</h1>
-                    <span className='text-sm'>15 jul 2024 - 4 jun 2025</span>
-                </div>
+                <ExperienciaCard 
+                    name={"Soluciones Integrales JB"}
+                    period={"15 jul 2024 - 4 jun 2025"}
+                    image={SolucionesIntegralesJB}
+                />
             </div>
-
-            {/* Contenedor en GRID para cuando este en mas empresas */}
-            {/* <div className='grid md:grid-cols-4 sm:grid-cols-2'>
-                <div className='flex flex-col items-center mt-3'>
-                    <img src={BeniElectricSac} alt="Beni Electric SAC" className='rounded-full w-36 h-36 border-2 border-gray-200'/>
-                    <h1>Corporación Beni Electric Sac</h1>
-                </div>
-
-                <div className='flex flex-col items-center mt-3'>
-                    <img src={SolucionesIntegralesJB} alt="Soluciones Integrales JB" className='rounded-full w-36 h-36 border-2 border-gray-200'/>
-                    <h1>Soluciones Integrales JB</h1>
-                </div>
-            </div> */}
-
-            
         </div>
     )
 }
