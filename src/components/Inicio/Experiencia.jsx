3bdd50be46b4798c23d8647f4c292d792a918b23
CarlosDaniel-090304 <carlosdanielchilcon19@gmail.com>
2026-01-10 21:09:49 -0500

    Update experiencia cargada dinamicamente

--- src/components/Inicio/Experiencia.jsx
+++ src/components/Inicio/Experiencia.jsx
@@ -1,33 +1,29 @@
-import React from 'react';
-// Importando imagenes
-import BeniElectricSac from '../../assets/BeniElectricSac.png';
-import SolucionesIntegralesJB from '../../assets/SolucionesIntegralesJB.jpeg';
+import React from 'react'
+import TitleComponent from '../TitleComponent'
+import ExperienciaCard from '../ExperienciaCard'
 
-// Componentes
-import TitleComponent from '../TitleComponent';
-import ExperienciaCard from '../ExperienciaCard';
+import { experienciaItems } from '../../lib/experiencia'
 
 function Experiencia(){
     return(
         <div className='flex flex-col gap-5'>
-            {/* Titulo */}
             <div>
                 <TitleComponent title={"Experiencia"} />
             </div>
 
-            {/* Experiencia */}
             <div className='flex justify-around text-gray-200 gap-3'>
-                <ExperienciaCard 
-                    name={"Corporación Beni Electric SAC"}
-                    period={"1 sept 2025 - 16 dic 2025"}
-                    image={BeniElectricSac}
-                />
-
-                <ExperienciaCard 
-                    name={"Soluciones Integrales JB"}
-                    period={"15 jul 2024 - 4 jun 2025"}
-                    image={SolucionesIntegralesJB}
-                />
+                {
+                    experienciaItems.map((items) => {
+                        return(
+                            <ExperienciaCard 
+                                key={items.id}
+                                name={items.name}
+                                period={items.period}
+                                image={items.image}
+                            />
+                        );
+                    })
+                }
             </div>
         </div>
     )
---
+++ src/lib/experiencia.js
@@ -0,0 +1,17 @@
+import BeniElectricSac from '../assets/BeniElectricSac.png'
+import SolucionesIntegralesJB from '../assets/SolucionesIntegralesJB.jpeg'
+
+export const experienciaItems = [
+    {
+        id: "1",
+        name: "Corporación Beni Electric SAC",
+        period: "1 sept 2025 - 16 dic 2025",
+        image: BeniElectricSac
+    },
+    {
+        id: "2",
+        name: "Soluciones Integrales JB",
+        period: "15 jul 2024 - 4 jun 2025",
+        image: SolucionesIntegralesJB
+    }
+];
