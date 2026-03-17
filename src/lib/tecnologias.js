866f567a72a17123c89e1d0247df7b2c571169ae
CarlosDaniel-090304 <carlosdanielchilcon19@gmail.com>
2026-03-17 16:32:37 -0500

    fix: Actualizando tecnologias

--- src/lib/tecnologias.js
+++ src/lib/tecnologias.js
@@ -1,11 +1,11 @@
 import React from '../assets/React Logo.png'
 import Nodejs from '../assets/Nodejs Logo.png'
-import Laravel from '../assets/Laravel Logo.png'
+import TypeScript from '../assets/TypeScriptLogo.png'
 import Php from '../assets/php Logo.png'
 import JavaScript from '../assets/JavaScript Logo.png'
 import MySql from '../assets/MySql Logo.png'
 import MongoDB from '../assets/MongoDB Logo.png'
-import Laragon from '../assets/Laragon Logo.jpeg'
+import Laravel from '../assets/Laravel Logo.png'
 import Docker from '../assets/Docker Logo.png'
 import VueJS from '../assets/VueJS.png'
 import Python from '../assets/Python Logo.jpeg'
@@ -32,40 +32,41 @@ export const tecnologias = [
     },
     {
         id : "4",
-        title : "Laravel",
-        image : Laravel,
-        alt : "Laravel"
+        title : "TypeScript",
+        image : TypeScript,
+        alt : "TypeScript"
     },
     {
         id : "5",
-        title : "PHP",
-        image : Php,
-        alt : "PHP"
-    },
-    {
-        id : "6",
         title : "JavaScript",
         image : JavaScript,
         alt : "JavaScript"
     },
     {
+        id : "6",
+        title : "Laravel",
+        image : Laravel,
+        alt : "Laravel"
+    },
+    {
         id : "7",
+        title : "PHP",
+        image : Php,
+        alt : "PHP"
+    },
+    {
+        id : "8",
         title : "MySql",
         image : MySql,
         alt : "MySql"
     },
     {
-        id : "8",
+        id : "9",
         title : "MongoDB",
         image : MongoDB,
         alt : "MongoDB"
     },
-    {
-        id : "9",
-        title : "Laragon",
-        image : Laragon,
-        alt : "Laragon"
-    },
+    
     {
         id : "10",
         title : "Docker",
